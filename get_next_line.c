0b3d2dc23b1e20b951bd79c055304e751f279b32
pc42 <ignacio.viseras@gmail.com>
2024-01-15 20:25:53 +0100

    qwe

--- get_next_line.c
+++ get_next_line.c
@@ -45,6 +45,22 @@ char	*ft_strjoin(char const *s1, const char *s2)
 	return (str); //no va el free de s1
 }
 
+// size_t	have_n(char *dst, char *src)
+// {
+// 	size_t	src_lenght;
+// 	size_t	index;
+
+// 	index = 0;
+// 	src_lenght = ft_strlen(src);
+// 	while (src[index] != '\0' && src[index] != '\n')
+// 	{
+// 		dst[index] = src[index];
+// 		index++;
+// 	}
+// 	dst[index] = '\0';
+// 	return (src_lenght);
+// }
+
 static char	*read_file(int fd)
 {
 	char *all_text;//lo q queda por leer
@@ -53,27 +69,35 @@ static char	*read_file(int fd)
 	static char *txt;//guardado
 	int len_sentence;
 	
-	all_text = malloc(BUFFER_SIZE * sizeof(char));
-	txt = "";
+	// char *p;
+
+	all_text = calloc(BUFFER_SIZE + 1, sizeof(char));
+	// *all_text = '\0';
+	if (!txt)
+		txt = "";
 	len_sentence = 0;
 	status = read(fd, all_text, BUFFER_SIZE);
 	while (status > 0)
 	{
 		if (!ft_strchr(all_text, '\n')){
-			len_sentence += ft_strchr(all_text, '\n') - all_text;
+			// printf("\n all %s", txt);
+			len_sentence = ft_strchr(all_text, '\n') - all_text;
 			sentence = ft_substr(all_text, 0, len_sentence);//lo que pintaria la cadena
 			txt = ft_strjoin(txt, sentence);
 			status = read(fd, all_text, BUFFER_SIZE);
 		
 		}
-		else
+		else // "\nco "  "tal\nco"
 		{
-			len_sentence = ft_strlen(all_text);
+			printf("\n all %s", all_text);
+			len_sentence = ft_strchr(all_text, '\n') - all_text;
 			sentence = ft_substr(all_text, 0, len_sentence);//lo que pintaria la cadena
-			txt = ft_strjoin(txt, sentence);
+			txt = ft_strjoin(txt, all_text + len_sentence);
+			// return (all_text);
+			
 			break;
 		}
-		printf("\ntxt %s", txt);
+		// printf("\ntxt %s", txt);
 	}
 	return (txt);
 }
@@ -93,7 +117,29 @@ char	*get_next_line(int fd)
 
 int	main ()
 {
+
+	// printf("Cadena %s", have_n("qwe"));
 	char file[] = "./prueba.txt";
-	get_next_line(open(file, O_RDONLY));
+	int a;
+
+	a = open(file, O_RDONLY);
+	get_next_line(a);
+	get_next_line(a);
+	close(a);
 	return (0);
 }
+
+
+
+
+
+/*
+
+hola\n
+que tal
+
+
+
+
+
+*/
--- get_next_line.h
+++ get_next_line.h
@@ -17,14 +17,14 @@
 # define GET_NEXT_LINE_H
 
 #ifndef BUFFER_SIZE
-#define BUFFER_SIZE 25
+#define BUFFER_SIZE 3
 #endif
 
 # include <unistd.h>
 # include <stdio.h>
 # include <fcntl.h>
 # include <stdlib.h>
-// # include <unistd.h>
+// # include <string.h>
 
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
